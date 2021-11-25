0654547725a4ea1f7086f5577fc243ca2fbb4d53
typhoonese <typhoonese@outlook.com>
2021-11-25 23:47:28 +0000

    Complete recursion and improve commentary

--- decoder.py
+++ decoder.py
@@ -1,27 +1,29 @@
+# Implements a recursive binary search approach to decode a given encoded value to readable form
+# The encoding algorithm can be found in def encode.
+
 from random import *
 
 
-def encode(asciiCode, n):  # encodes x.....x (n times) where asciiCode represents x
+def encode(asciiCode, n):  # encodes .....x (n chars) where asciiCode represents x and all chars before are " "
     targetEncoding = 0
-
     for i in range(n-1):
-        targetEncoding += (256 ** i) * (32)
+        targetEncoding += (256 ** i) * (ord(" "))
     targetEncoding += (256 ** (n-1)) * (asciiCode)
     return targetEncoding
 
 
-def encodeChar(asciiCode, n):  # encodes the value of a particular ascii code at particular position n
+def encodeChar(asciiCode, n):  # encodes the value of a particular ascii code at position n
     return (256 ** n) * asciiCode
 
 
-def translateAscii(asciiCodes):  # translates ascii codes to readable form
+def translateAscii(asciiCodes):  # translates ascii codes (arr) to readable form
     decodedPhrase = ""
     for code in asciiCodes:
         decodedPhrase += str(chr(code))
     return decodedPhrase
 
 
-# returns true if target encoding is bigger than temp encoding
+# returns true if target encoding is bigger than or equal to temp encoding
 def isDiffBigger(asciiCode, guessLength, targetEncoding):
     tempEncoding = encode(asciiCode, guessLength)
     if (targetEncoding - tempEncoding >= 0):
@@ -31,7 +33,7 @@ def isDiffBigger(asciiCode, guessLength, targetEncoding):
 
 
 # finds the ascii code that minimises the encoding difference start : min ascii code, end: max ascii code, guessLength: length of encoded phrase
-def find(start, end, targetEncoding, guessLength, asciiChars):
+def decode(start, end, targetEncoding, guessLength):
 
     if (end >= start and guessLength > 0):
         asciiCode = (end+start) // 2
@@ -47,37 +49,43 @@ def find(start, end, targetEncoding, guessLength, asciiChars):
 
         # diff needs to get smaller
         if(not isDiffBigger(asciiCode, guessLength, targetEncoding)):
-            return find(start, asciiCode-1, targetEncoding, guessLength, asciiChars)
+            return decode(start, asciiCode-1, targetEncoding, guessLength)
 
         # should find the first ascii char that makes targetEncoding-tempEncoding positive
         if (isDiffBigger(asciiCode, guessLength, targetEncoding)):
             # need to check if asciiCode is the first positive diff
             if(isDiffBigger(asciiCode+1, guessLength, targetEncoding)):
                 # diff needs to get bigger
-                return find(asciiCode+1, end, targetEncoding, guessLength, asciiChars)
+                return decode(asciiCode+1, end, targetEncoding, guessLength)
             else:
-                asciiChars.append(asciiCode)
-                return asciiCode, targetEncoding - encodeChar(asciiCode, guessLength-1), guessLength-1, asciiChars
+                return asciiCode, targetEncoding - encodeChar(asciiCode, guessLength-1), guessLength-1
     else:
         return -1
 
 
 def main():
 
-    start = 32  # ascii code
-    end = 127  # ascii code
-    # targetEncoding = 391565397484887
-    # guessLength = 6  # number of letters encoded
-    targetEncoding = 87
-    guessLength = 1
-    asciiChars = []  # store the decoded asci codes
+    # encoded value - 157709172775299964495191
+    targetEncoding = int(input("Input the encoded value to be decoded: "))
+    # estimated length - 10. TODO: can be automated instead of hardcoding
+    guessLength = int(input("Guess the length of decoded phrase: "))
 
-    result = find(start, end, targetEncoding, guessLength, asciiChars)
-    print("In main: ", result)
+    # min possible ascii code in decoded phrase
+    start = ord(" ")
+    # max possible ascii code in decoded phrase
+    end = ord("~")
+    # store the decoded asci codes in reverse order
+    asciiChars = []
 
-    if(not result == -1):
-        (asciiCode, diff, left, arr) = result
-        print("char: ", translateAscii(arr))
+    while (guessLength > 0):
+        result = decode(start, end, targetEncoding, guessLength)
+        if(not result == -1):
+            (asciiCode, targetEncoding, guessLength) = result
+            asciiChars.append(asciiCode)
+        else:
+            print("Exited with -1")
+
+    print("Decoded phrase: ", translateAscii(asciiChars)[::-1])
 
 
 if (__name__ == "__main__"):

21407dcc4574abcbe078a31b0cfd4e48a9d1ff36
typhoonese <typhoonese@outlook.com>
2021-11-25 21:11:33 +0000

    Find the last digit successfully - WIP recursion

--- decoder.py
+++ decoder.py
@@ -1,11 +1,21 @@
 from random import *
 
+# encodes x.....x (n times) where asciiCode represents x
+
 
 def encode(asciiCode, n):
-    encodedVal = 0
+    targetEncoding = 0
     for i in range(n):
-        encodedVal += (256 ** i) * asciiCode
-    return encodedVal
+        targetEncoding += (256 ** i) * asciiCode
+    return targetEncoding
+
+# encodes the value of a particular ascii code at particular position n
+
+
+def encodeChar(asciiCode, n):
+    return (256 ** n) * asciiCode
+
+# translates ascii to readable form
 
 
 def translateAscii(asciiChars):
@@ -15,24 +25,42 @@ def translateAscii(asciiChars):
     return decodedPhrase
 
 
-def find(start, end, encodedVal):
+def find(start, end, targetEncoding, guessLength, asciiChars):
 
-    if (end > start):
+    if (end > start and guessLength > 0):
         mid = (end-start) // 2 + start
-        computedEn = encode(mid, 10)
+        tempEncoding = encode(mid, guessLength)
 
-        if(computedEn - encodedVal >= 0):
-            return find(start, mid, encodedVal)
+        print("targetEncoding  : ", targetEncoding)
+        print("tempEncoding    : ", tempEncoding)
+        print("diff            : ", targetEncoding - tempEncoding)
+        print("start: ", start)
+        print("end  : ", end)
+        print("mid  : ", mid)
+        print("*******************************")
 
-        if (encodedVal - computedEn >= 0):
-            return mid, encodedVal - computedEn
+        if(tempEncoding - targetEncoding >= 0):
+            return find(start, mid, targetEncoding, guessLength, asciiChars)
+
+        if (targetEncoding - tempEncoding >= 0):
+            asciiChars.append(mid)
+            return mid, targetEncoding - encodeChar(mid, guessLength-1), guessLength-1, asciiChars
     else:
         return -1
 
 
 def main():
 
-    print("In main: ", find(32, 127, 157709172775299964495191))
+    guessLength = 9
+    start = 32
+    end = 127
+    targetEncoding = 1871078840601672443223
+    asciiChars = []
+
+    (x, y, z, t) = find(start, end, targetEncoding, guessLength, asciiChars)
+    print("In main: ", x, y, z, t)
+
+    print("Char: ", translateAscii(t))
 
 
 if (__name__ == "__main__"):

cd7762321e676357c824f2f097e157e920dea8fa
typhoonese <typhoonese@outlook.com>
2021-11-26 18:40:21 +0000

    Improve commentary in checkEncoding.py

--- checkEncoding.py
+++ checkEncoding.py
@@ -1,27 +1,30 @@
-#checkEncoding checks whether plainText can be encoded into the targetEncodedVal
-#when encoded with the encoding algorithm
-def checkEncoding (plainText, targetEncodedVal):
-  #encodedPlainText: to store the value of encoded plainText
-  encodedPlainText = 0
-  #number: index to loop through plainText
-  number = 0
-  
-  #Encode the plain text 
-  while number < len(plainText) :
-  #Convert each index of plainText to ASCII and encode with respect to the algorithm provided
-   encodedPlainText  += (256 ** number) * ord(plainText[number])
-   number += 1 
+# checkEncoding checks whether plainText can be encoded into the targetEncoding
+# when encoded with the encoding algorithm
+def checkEncoding(plainText, targetEncoding):
+    # encodedPlainText: to store the value of encoded plainText
+    encodedPlainText = 0
+    # number: index to loop through plainText
+    number = 0
 
-  print ("Encoded Plain Text   : ", targetEncodedVal)
-  print ("Target Encoded Value : ", encodedPlainText)
-  print ("Target Encoded Value - Encoded Plain Text: ", targetEncodedVal - encodedPlainText)
+    # Encode the plain text
+    while number < len(plainText):
+        # Convert each index of plainText to ASCII and encode with respect to the algorithm provided
+        encodedPlainText += (256 ** number) * ord(plainText[number])
+        number += 1
 
-  if (encodedPlainText == targetEncodedVal):
-   return plainText
+    # print("Encoded Plain Text   : ", targetEncoding)
+    # print("Target Encoded Value : ", encodedPlainText)
+    # print("Target Encoded Value - Encoded Plain Text: ",
+    #       targetEncoding - encodedPlainText)
 
-#Enter plain text here
-plainText = "Well done!"
-#Enter the encoded phrase here
-targetEncodedVal = 157709172775299964495191
-#Check if plainText encodes to the targetEncodedVal. Returns the value of plain text if successful. Returns none otherwise
-print("Correct plain text found: ", checkEncoding (plainText, targetEncodedVal))
+    if (encodedPlainText == targetEncoding):
+        return plainText
+
+
+# Enter plain text here
+plainText = input("Input plain text: ")
+# encoded value - 157709172775299964495191
+targetEncoding = int(input("Input the encoded value to be decoded: "))
+
+# Check if plainText encodes to the targetEncoding. Returns the value of plain text if successful. Returns none otherwise
+print("Correct plain text found: ", checkEncoding(plainText, targetEncoding))
